94512e5c4b9e5ee1e16818525895ec01db237500
Isaac Wise <38229170+IsaacWise06@users.noreply.github.com>
2023-05-18 18:22:40 +0000

    properly sort leaderboard

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -10,14 +10,11 @@ const Leaderboard = () => {
       <section className='w-full max-w-md'>
         <h1 className='text-gray-100 text-3xl text-semibold pb-2'>Leaderboard</h1>
         <div className='flex flex-col gap-2'>
-          {leaderboard?.map((group, index) => (
+          {leaderboard?.sort((a,b) => a.points > b.points ? 0 : 1).map((group, index) => (
             <div key={index} className='w-full bg-slate-600 rounded-md p-2 flex items-center justify-between flex-row gap-2'>
               <div className='flex items-center gap-2'>
                 <div className='rounded-full flex items-center justify-center h-12 w-12 p-2 bg-slate-700'>
-                  {/*
-                    Need to sort these manually then use the index as a rank
-                  */}
-                  <span className='text-gray-100 font-bold text-2xl'>{group.rank}</span>
+                  <span className='text-gray-100 font-bold text-2xl'>{index + 1}</span>
                 </div>
                 <span className='text-gray-100 font-semibold text-lg'>Class of {group.name}</span>
               </div>
