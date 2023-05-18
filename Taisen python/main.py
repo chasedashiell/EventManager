048af0ab9bd7e0b4cc6a9dcee9b16bbc075b6942
Isaac Wise <38229170+IsaacWise06@users.noreply.github.com>
2023-05-18 14:21:55 -0500

    Update points route

--- Taisen python/main.py
+++ Taisen python/main.py
@@ -2,6 +2,7 @@ import csv
 import json
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
+import pandas as pd
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -19,11 +20,19 @@ def csv_to_json(csvFilePath):
     jsonString = json.dumps(jsonArray, indent=4)
     return(jsonString)
 
-print(csv_to_json(r'classes.csv'))
-
-@app.route('/leaderboard')
+@app.route('/leaderboard', methods=['GET', 'POST'])
 def points():
     if request.method == 'GET':
         return csv_to_json(r'classes.csv')
+    if request.method == 'POST':
+        # Serialize the data into a dictionary
+        request_data = json.loads(request.data)
+        # Read the CSV
+        leaderboard = pd.read_csv(r'classes.csv')
+        # Update the value
+        leaderboard.loc[leaderboard['name'] == request_data['name'], 'points'] = request_data['amount']
+        # Save the CSV
+        leaderboard.to_csv(r'classes.csv', index=False)
+        return request_data
 
 app.run(host='0.0.0.0', port=5000, debug=True)
