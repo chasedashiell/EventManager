84d0086222242d4bc0d7a8cbef19350a91af260f
Isaac Wise <38229170+IsaacWise06@users.noreply.github.com>
2023-05-22 19:18:02 +0000

    Fix sorting

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -3,20 +3,19 @@ import getLeaderboard from '../api/requests/getLeaderboard';
 
 const Leaderboard = () => {
   const { data: leaderboard } = getLeaderboard()
-  console.log(leaderboard)
   
   return (
     <div className='w-full h-screen flex justify-center px-2 items-center'>
       <section className='w-full max-w-md'>
         <h1 className='text-gray-100 text-3xl text-semibold pb-2'>Leaderboard</h1>
         <div className='flex flex-col gap-2'>
-          {leaderboard?.sort((a,b) => a.points > b.points ? 0 : 1).map((group, index) => (
+          {leaderboard?.sort((a,b) => b.points - a.points ).map((group, index) => (
             <div key={index} className='w-full bg-slate-600 rounded-md p-2 flex items-center justify-between flex-row gap-2'>
               <div className='flex items-center gap-2'>
                 <div className='rounded-full flex items-center justify-center h-12 w-12 p-2 bg-slate-700'>
                   <span className='text-gray-100 font-bold text-2xl'>{index + 1}</span>
                 </div>
-                <span className='text-gray-100 font-semibold text-lg'>Class of {group.name}</span>
+                <span className='text-gray-100 font-semibold text-lg'>Class of {group.grade}</span>
               </div>
               <span className='text-gray-100 text-lg font-semibold'>{group.points} Points</span>
             </div>
